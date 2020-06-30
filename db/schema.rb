efb64dd2bb69b400e836662a05c10082206bbb03
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-06-30 15:17:31 +0800

    步驟12: 新增任務標題不可為空的migration檔案，設定資料庫限制

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_154631) do
+ActiveRecord::Schema.define(version: 2020_06_30_070529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
