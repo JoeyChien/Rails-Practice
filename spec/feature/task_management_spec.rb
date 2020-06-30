39d6ef2fbb5d5392108189236f1c8dd2c8f8252d
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-06-30 12:34:31 +0800

    步驟9: 修正多餘換行及空白

--- spec/feature/task_management_spec.rb
+++ spec/feature/task_management_spec.rb
@@ -12,7 +12,7 @@ RSpec.feature "Task management" do
     
     expect(page).to have_text(I18n.t('message.create_success'))
     expect(task_last.title).to eq ("My title") 
-    expect(task_last.content).to eq ("My content")     
+    expect(task_last.content).to eq ("My content")
   end
 
   scenario "User edit a task" do
@@ -26,8 +26,8 @@ RSpec.feature "Task management" do
     updated_task = Task.find(task.id)
 
     expect(page).to have_text(I18n.t('message.update_success'))
-    expect(updated_task.title).to eq ("Edit my title") 
-    expect(updated_task.content).to eq ("Edit my content")    
+    expect(updated_task.title).to eq ("Edit my title")
+    expect(updated_task.content).to eq ("Edit my content")
   end
 
   scenario "User delete a task" do
@@ -37,6 +37,6 @@ RSpec.feature "Task management" do
     click_link I18n.t('action.delete')
 
     expect(page).to have_text(I18n.t('message.delete_success'))
-    expect{Task.find(task.id)}.to raise_exception(ActiveRecord::RecordNotFound)    
+    expect{Task.find(task.id)}.to raise_exception(ActiveRecord::RecordNotFound)
   end
 end
