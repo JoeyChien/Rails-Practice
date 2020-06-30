6184645ce41de92debf116e217955a2be073c6dd
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-06-30 12:02:28 +0800

    步驟8: 在任務新刪修功能的feature spec中新增DB檢查

--- spec/feature/task_management_spec.rb
+++ spec/feature/task_management_spec.rb
@@ -8,7 +8,11 @@ RSpec.feature "Task management" do
     fill_in "任務內容", with: "My content"
     click_button 'Create Task'
 
+    task_last = Task.last
+    
     expect(page).to have_text("新增成功")
+    expect(task_last.title).to eq ("My title") 
+    expect(task_last.content).to eq ("My content") 
   end
 
   scenario "User edit a task" do
@@ -18,8 +22,12 @@ RSpec.feature "Task management" do
     fill_in "任務名稱", with: "Edit my title"
     fill_in "任務內容", with: "Edit my content"
     click_button 'Update Task'
+    
+    updated_task = Task.find(task.id)
 
     expect(page).to have_text("修改成功")
+    expect(updated_task.title).to eq ("Edit my title") 
+    expect(updated_task.content).to eq ("Edit my content") 
   end
 
   scenario "User delete a task" do
@@ -29,5 +37,6 @@ RSpec.feature "Task management" do
     click_link '刪除'
 
     expect(page).to have_text("刪除成功")
+    expect{Task.find(task.id)}.to raise_exception(ActiveRecord::RecordNotFound)
   end
 end
