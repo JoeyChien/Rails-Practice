f17f5dd23753eb656f691cb15e54c507837bbc7f
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-04 22:53:53 +0800

    步驟12: 調整任務的model spec寫法及任務的model中標題必填設定

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  before(:each) do
-    @task = Task.new
+  let(:task) { create(:task) }
+
+  before do
+    task
   end
 
   it "it contain title & content" do
-    @task.title
-    @task.content
+    task.title
+    task.content
   end
 
   it "is not valid without a title" do
-    @task.title = nil
-    expect(@task).to_not be_valid
+    task.title = nil
+    expect(task).to_not be_valid
   end
-
 end
