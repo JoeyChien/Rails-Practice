db9c5e100a10eb27986adc2b5ef44d45bef6e925
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-15 16:40:16 +0800

    步驟15: 建立任務狀態欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_064519) do
+ActiveRecord::Schema.define(version: 2020_07_14_092402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_064519) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "end_time"
+    t.integer "status", default: 0, null: false
   end
 
 end
