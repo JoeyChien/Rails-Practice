fc5709e1707d3f4b2497391076ad3c7183aaf2bc
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-22 01:24:28 +0800

    步驟15: 任務狀態的選項 value 使用 enum，增加可讀性

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   enum status: [ :pending, :inprogress, :completed ]
-  scope :filter_by_status, -> (status) { where status: status }
+  scope :filter_by_status, -> (status) { where(status: Task.statuses[status]) }
   scope :query_by_title, -> (keyword) { where("title ILIKE ?", "%#{keyword}%")}
 end
