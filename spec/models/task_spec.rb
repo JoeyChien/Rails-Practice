176437d1bb4d95e3c970bc11e8b52ae5f64181ad
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-22 01:44:56 +0800

    步驟15: model spec中任務狀態使用 enum，增加可讀性，修正針對 scope 搜尋的測試架構

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -19,23 +19,12 @@ RSpec.describe Task, type: :model do
   end
 
   describe "search for task" do
-    let(:inprogress_task) { create(:task, title: "My first task title", status: 1) }
-    let(:pending_task) { create(:task, title: "My second task title", status: 0) }
+    let(:inprogress_task) { create(:task, title: "My first task title", status: 'inprogress') }
+    let(:pending_task) { create(:task, title: "My second task title", status: 'pending') }
 
     before do
       inprogress_task
       pending_task
-    end
-
-    it "find by task title & status" do
-      result = Task.query_by_title("task title").filter_by_status(1)
-      expect(result).to eq([inprogress_task])
-    end
-
-    it "not found by task title & status" do
-      result = Task.query_by_title("My first task title")
-      result = result.filter_by_status(0)
-      expect(result).to be_empty
     end
 
     it "find by task title" do
@@ -48,12 +37,49 @@ RSpec.describe Task, type: :model do
     end
 
     it "find by task status" do
-      result = Task.filter_by_status(0)
+      result = Task.filter_by_status('pending')
       expect(result).to eq([pending_task])
     end
 
     it "not found by task status" do
-      expect(Task.filter_by_status(2)).to be_empty
+      expect(Task.filter_by_status('completed')).to be_empty
+    end
+  end
+  describe '.query_by_title' do
+    let(:first_task) { create(:task, title: "My first task title") }
+    let(:second_task) { create(:task, title: "My second task title") }
+
+    before do
+      first_task
+      second_task
+    end
+
+    it "find by task title" do
+      result = Task.query_by_title("first task")
+      expect(result).to eq([first_task])
+    end
+
+    it "not found by task title" do
+      expect(Task.query_by_title("not")).to be_empty
+    end   
+  end
+
+  describe '.filter_by_status' do   
+    let(:inprogress_task) { create(:task, title: "My inprogress task title", status: 'inprogress') }
+    let(:pending_task) { create(:task, title: "My pending task title", status: 'pending') }
+
+    before do
+      inprogress_task
+      pending_task
+    end
+
+    it "find by task status" do
+      result = Task.filter_by_status('pending')
+      expect(result).to eq([pending_task])
+    end
+
+    it "not found by task status" do
+      expect(Task.filter_by_status('completed')).to be_empty
     end
   end
 end
