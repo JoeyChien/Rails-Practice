bc140556dfc77b05791eb05d59f038b2badf741f
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-21 15:20:27 +0800

    步驟15: 任務的狀態及標題的model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -17,4 +17,43 @@ RSpec.describe Task, type: :model do
     expect(task).to_not be_valid
     expect(task.errors.full_messages.first).to match("#{I18n.t('activerecord.attributes.task.title')} #{I18n.t('activerecord.errors.models.task.attributes.title.blank')}")
   end
+
+  describe "search for task" do
+    let(:inprogress_task) { create(:task, title: "My first task title", status: 1) }
+    let(:pending_task) { create(:task, title: "My second task title", status: 0) }
+
+    before do
+      inprogress_task
+      pending_task
+    end
+
+    it "find by task title & status" do
+      result = Task.query_by_title("task title").filter_by_status(1)
+      expect(result).to eq([inprogress_task])
+    end
+
+    it "not found by task title & status" do
+      result = Task.query_by_title("My first task title")
+      result = result.filter_by_status(0)
+      expect(result).to be_empty
+    end
+
+    it "find by task title" do
+      result = Task.query_by_title("first task")
+      expect(result).to eq([inprogress_task])
+    end
+
+    it "not found by task title" do
+      expect(Task.query_by_title("not")).to be_empty
+    end
+
+    it "find by task status" do
+      result = Task.filter_by_status(0)
+      expect(result).to eq([pending_task])
+    end
+
+    it "not found by task status" do
+      expect(Task.filter_by_status(2)).to be_empty
+    end
+  end
 end
