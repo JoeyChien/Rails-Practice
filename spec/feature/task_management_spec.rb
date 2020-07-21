c312875a27b5707e9a5a0dbbd8bad61a5a1df8aa
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-07-21 17:18:27 +0800

    步驟15: 任務搜尋功能新增feature spec

--- spec/feature/task_management_spec.rb
+++ spec/feature/task_management_spec.rb
@@ -38,7 +38,7 @@ RSpec.feature 'Task management' do
     end   
   end
 
-  feature 'Task CRUD ' do
+  feature 'Task CRUD' do
     scenario "User create a new task" do
       visit new_task_path
   
@@ -79,6 +79,49 @@ RSpec.feature 'Task management' do
     end
   end
 
+  feature 'Task search' do
+    context 'Search by title & status' do
+      let(:inprogress_task) { create(:task, title: "My inprogress task", status: 1) }
+      let(:pending_task) { create(:task, title: "My pending task", status: 0) }
+
+      before do
+        inprogress_task
+        pending_task
+      end
+
+      scenario "Search by title & status" do
+        visit tasks_path
+       
+        find("input.query-title").set "task"
+        select I18n.t("enums.task.statuses.pending"), from: 'status'
+        click_button I18n.t('helpers.search')
+
+        expect_search('title', pending_task.title)
+        expect_search('content', pending_task.content)
+      end
+
+      scenario "Search by status" do
+        visit tasks_path
+       
+        select I18n.t("enums.task.statuses.inprogress"), from: 'status'
+        click_button I18n.t('helpers.search')
+
+        expect_search('title', inprogress_task.title)
+        expect_search('content', inprogress_task.content)
+      end
+
+      scenario "Search by title" do
+        visit tasks_path
+       
+        find("input.query-title").set "inprogress task"
+        click_button I18n.t('helpers.search')
+
+        expect_search('title', inprogress_task.title)
+        expect_search('content', inprogress_task.content)
+      end
+    end   
+  end
+
   private
   
   def expect_order(column, fisrt_task, second_task)
@@ -88,4 +131,13 @@ RSpec.feature 'Task management' do
     end
     expect(@tasks).to eq [fisrt_task, second_task]
   end
+
+  def expect_search(column, task)
+    @tasks = []
+    page.all(".task-#{column}").each do | element |
+      @tasks << element.text
+    end
+    expect(@tasks).to eq [task]
+  end
+
 end
